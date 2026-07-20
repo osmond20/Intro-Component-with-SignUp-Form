3564182f68c27dc5b2042062ec0f2f106a993794
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-07-20 15:00:29 +0200

    ----

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// selecting all input elements in the form with CSS selector of '.inputs'
 const input = document.querySelectorAll(".inputs");
+// selecting the button with the CSS selector of '.claim-button'
 const button = document.querySelector(".claim-button");
 
