449cf5703a8e7a759d09b1daba845e0c57002fef
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-07-22 08:24:07 +0200

    ----

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// selecting the form
+const form = document.querySelector('.sign-up-form');
 // selecting all input elements in the form with CSS selector of '.inputs'
 const input = document.querySelectorAll(".inputs");
 // selecting the button with the CSS selector of '.claim-button'
 const button = document.querySelector(".claim-button");
 
-button.addEventListener("click", () => {
-    
+button.addEventListener("click", event => {
+    event.preventDefault();
 })
 
