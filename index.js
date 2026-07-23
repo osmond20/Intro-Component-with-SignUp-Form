0a7e0cfc74f26be93a795197ee9b65ca90edd3cb
Nkosinathi Mothudi <osmondmothudi@gmail.com>
2026-07-23 22:15:50 +0200

    ------

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 // selecting the form
 const form = document.querySelector('.sign-up-form');
 // selecting all input elements in the form with CSS selector of '.inputs'
-const input = document.querySelectorAll(".inputs");
+const inputs = document.querySelectorAll('.inputs');
 // selecting the button with the CSS selector of '.claim-button'
-const button = document.querySelector(".claim-button");
+const button = document.querySelector('.claim-button');
 
-button.addEventListener("click", event => {
-    event.preventDefault();
-})
+// Email validation form function
+function isValidEmail(email){
+    if(typeof email !== "string"){
+        return false;
+    }
+
+    email = email.trim();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+    return emailRegex.test(email);
+}
+
+form.addEventListener('submit', event =>{
+
+    let isValid = true;
+
+    inputs.forEach(input =>{
+        const invalidEmail = input.type === 'email' && !isValidEmail(input.value); 
+        if(input.value.trim() == '' || invalidEmail){
+            input.classList.toggle('invalid');
+            isValid = false;
+        }
+        else{
+            input.classList.remove('invalid');
+        }
+    })
+
+    if(!isValid){
+        event.preventDefault();
+    }
+});
 
